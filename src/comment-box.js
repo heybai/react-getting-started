e04954d6f5992f6111381a5709973a2354c17b33
Ivan Grishchenko <heybai@yandex-team.ru>
2015-06-29 23:23:48 +0300

    implement comment form

--- src/comment-box.js
+++ src/comment-box.js
@@ -1,9 +1,27 @@
 var CommentBox = React.createClass({
-    loadCommentsFromServer: function() {
+    loadCommentsFromServer: function () {
         $.ajax({
             url: this.props.url,
             dataType: 'json',
             cache: false,
+            success: function (data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    handleCommentSubmit: function (comment) {
+        var comments = this.state.data;
+        var newComments = comments.concat([comment]);
+        this.setState({data: newComments});
+
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            type: 'POST',
+            data: comment,
             success: function (data) {
                 this.setState({data: data});
             }.bind(this),
@@ -25,7 +43,7 @@ var CommentBox = React.createClass({
             <div className="commentBox">
                 <h1>Comments</h1>
                 <CommentList data={this.state.data}/>
-                <CommentForm />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
             </div>
         );
     }
@@ -66,11 +84,33 @@ var Comment = React.createClass({
 });
 
 var CommentForm = React.createClass({
+    handleSubmit: function (e) {
+        console.info("creating comment...");
+
+        e.preventDefault();
+
+        var author = React.findDOMNode(this.refs.author).value.trim();
+        var text = React.findDOMNode(this.refs.text).value.trim();
+
+        if (!text || !author) {
+            return;
+        }
+
+        this.props.onCommentSubmit({author: author, text: text});
+
+        React.findDOMNode(this.refs.author).value = '';
+        React.findDOMNode(this.refs.text).value = '';
+
+        return;
+    },
+
     render: function () {
         return (
-            <div className="commentForm">
-                Hello, world! I am a CommentForm.
-            </div>
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Your name" ref="author"/>
+                <input type="text" placeholder="Say something..." ref="text"/>
+                <input type="submit" value="Post"/>
+            </form>
         );
     }
 });
