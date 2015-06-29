6d7597dbad9ce6e6deae200632b3cba0f03d7056
Ivan Grishchenko <heybai@yandex-team.ru>
2015-06-29 23:08:54 +0300

    load comments from file via ajax

--- src/comment-box.js
+++ src/comment-box.js
@@ -1,10 +1,30 @@
 var CommentBox = React.createClass({
+    loadCommentsFromServer: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function (data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+
+    getInitialState: function () {
+        return {data: []}
+    },
+    componentDidMount: function () {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
     render: function () {
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList data={this.props.data}/>
-
+                <CommentList data={this.state.data}/>
                 <CommentForm />
             </div>
         );
@@ -55,12 +75,7 @@ var CommentForm = React.createClass({
     }
 });
 
-var data = [
-    {author: "Peter", text: "This is one comment"},
-    {author: "Bob", text: "And this is *another one* comment"}
-];
-
 React.render(
-    <CommentBox data={data}/>,
+    <CommentBox url="comments.json" pollInterval={2000}/>,
     document.getElementById('content')
 );
